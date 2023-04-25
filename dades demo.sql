d3b34ace9fae98ce85082ded24e64a1808d51df2
Abdelbarie El Harrak <98111797+bari1909@users.noreply.github.com>
2023-04-25 17:19:26 +0200

    Update dades demo.sql

--- dades demo.sql
+++ dades demo.sql
@@ -5,7 +5,7 @@ INSERT INTO Treballador (id_treballador, nom, cognoms, rol, data_incorporacio)
 VALUES (2, 'Maria', 'Garcia', 'Supervisor', '2019-05-01');
 
 INSERT INTO Maquina (id_maquina, nom, descripcio)
-VALUES (1, 'Maquina 1', 'Maquina d\'estampació');
+VALUES (1, 'Maquina 1', "Maquina d'estampació");
 
 INSERT INTO Maquina (id_maquina, nom, descripcio)
 VALUES (2, 'Maquina 2', 'Maquina de tall');
@@ -14,4 +14,4 @@ INSERT INTO Errors (id_treballador, id_maquina, estat_error, tipus, descripcio)
 VALUES (1, 1, 'Pendent', 'Error de configuració', 'La velocitat de la màquina és massa elevada');
 
 INSERT INTO Errors (id_treballador, id_maquina, estat_error, tipus, descripcio)
-VALUES (2, 2, 'Resolt', 'Error mecànic', 'La taula de tall s\'ha desviat de la posició correcta');
+VALUES (2, 2, 'Resolt', 'Error mecànic', "La taula de tall s'ha desviat de la posició correcta");
